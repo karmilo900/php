3e83068bf6f607e165502944129ca20f4325b0ef
karmilo900 <camilopaduamayordomo@gmail.com>
2023-06-01 21:29:31 -0500

    terminando arreglos

--- arreglos.php
+++ arreglos.php
@@ -7,7 +7,7 @@ $dia[4]="friday";
 $dia[5]="fsaturday";
 $dia[6]="sunday";
 
-
+/*
 echo "<br>";
 
 
@@ -24,5 +24,34 @@ for ($i=0; $i < $total; $i++) {
 	}
 }
 
+//arreglos asociativos
+$registros = array('Docu'=> 1000349464,'Nombre'=> "Camilo", 'Direccion'=> "Mi Casa");
+echo "<br>";
+echo $registros['Docu'];
+echo "<br>";
+
+$articulos = array('Codigo'=> 100,'Nombrep'=> "camiseta tipo polo importada desde berlin alemania con hilos de ceda fina de la india.",'Precio'=> 8000);
+
+foreach ($articulos as $key => $value) {
+	echo " para la llave: ".$key." su valor es:".$value;
+	echo "<br>";
+}
+
+$aprendices= array('Docu'=>28540, 'NombreA'=>"Edan",'programa'=> "Analisis y Desarrollo de Software e inteligencia artificial especializado en php nivel superior con especializacion",'trimestre'=>5);
+
+foreach($aprendices as $fi=> $valor){
+	echo "para los datos: ".$fi." su valor es: ".$valor;
+	echo "<br>";
+}
+*/
+$totaless=0;
+$numeros= array('n1' => 2,'n2' => 3,'n3' => 2,'n4' => 2,'n5' => 2, );
+foreach($numeros as $a=> $valores){
+
+	
+	$totaless+=$valores;
+
+}
+echo "el total de la suma es de: ".$totaless;
 
 ?>
